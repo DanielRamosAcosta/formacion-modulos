88518e85ae26083827cc740f3e3ab105159d1b21
Daniel Ramos <danielramosacosta@hotmail.com>
2018-08-09 10:27:10 +0200

    Probar lodash-es

--- 04-bundlers/src/index.js
+++
@@ -1,5 +0,0 @@
-// const { add } = require('./math.js')
-import { add } from "./math.js";
-
-// console.log(MyMath.add(5, 4))
-console.log(add(6, 2))
---
+++ 04-bundlers/src/index.mjs
@@ -0,0 +1,6 @@
+import { chunk } from "lodash-es";
+import { add } from "./math.mjs";
+
+// console.log(MyMath.add(5, 4))
+console.log(add(6, 2))
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 2))
